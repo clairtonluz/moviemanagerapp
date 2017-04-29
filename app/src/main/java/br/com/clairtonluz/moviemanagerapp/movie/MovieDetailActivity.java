2bef24d9dbc9b742869ca78162a9b81c029570e7
clairtonluz <clairton.c.l@gmail.com>
2017-04-29 17:03:18 -0300

    exibindo detalhes do filme

--- app/src/main/java/br/com/clairtonluz/moviemanagerapp/movie/MovieDetailActivity.java
+++ app/src/main/java/br/com/clairtonluz/moviemanagerapp/movie/MovieDetailActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -14,15 +15,16 @@ import br.com.clairtonluz.moviemanagerapp.generic.BackButtonActivity;
 public class MovieDetailActivity extends BackButtonActivity {
 
     private ImageView toolbarImage;
+    private TextView yearText;
+    private TextView descriptionText;
     private CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        toolbarImage = (ImageView) findViewById(R.id.toolbar_image);
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         setupToolbar();
+        initFields();
         Movie movie = getExtraSerializable("movie", Movie.class);
         showMovie(movie);
     }
@@ -31,14 +33,23 @@ public class MovieDetailActivity extends BackButtonActivity {
         collapsingToolbarLayout.setTitle(movie.getName());
         Picasso.with(this)
                 .load(movie.getUrlImage())
-                .placeholder(R.drawable.loading_spinner)
                 .error(R.drawable.default_image)
                 .into(toolbarImage);
+
+        descriptionText.setText(movie.getDescription());
+        yearText.setText(String.valueOf(movie.getYear()));
     }
 
     public void editAction(View view) {
         collapsingToolbarLayout.setTitle("teste");
         Snackbar.make(view, "Replace with your own actio2n", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
+    }
+
+    private void initFields() {
+        toolbarImage = (ImageView) findViewById(R.id.toolbar_image);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        yearText = (TextView) findViewById(R.id.year_text);
+        descriptionText = (TextView) findViewById(R.id.description_text);
     }
 }
